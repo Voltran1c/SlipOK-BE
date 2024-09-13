8b052b4e4d5ff95f304f7b3bf6c4cf1219a9a1fa
Watcharapong.K <147409902+Voltran1c@users.noreply.github.com>
2024-09-13 13:05:22 +0700

    edit code

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const axios = require("axios");
 const multer = require("multer");
 const FormData = require("form-data");
 const cors = require("cors");
+require("dotenv").config(); // Load environment variables
 
 const app = express();
-app.use(cors()); // allow req server
+app.use(cors()); // Allow requests from other domains
 const port = 5000;
 
 const storage = multer.memoryStorage();
@@ -13,17 +14,28 @@ const upload = multer({ storage: storage });
 
 app.post("/slipok", upload.single("files"), async (req, res) => {
   const file = req.file;
+
+  if (!process.env.API_URL) {
+    return res.status(500).send("API_URL environment variable is not set.");
+  }
+
+  if (!file) {
+    return res.status(400).send("No file uploaded.");
+  }
+
   const formData = new FormData();
   formData.append("files", file.buffer, file.originalname);
+
   try {
     const response = await axios.post(process.env.API_URL, formData, {
       headers: {
-        "Content-Type": file.mimetype,
+        ...formData.getHeaders(), // Correctly set the headers for form-data
         "x-authorization": process.env.API_KEY,
       },
     });
     res.json(response.data);
   } catch (error) {
+    console.error("Error posting to external API:", error);
     res.status(500).send(error.message);
   }
 });
@@ -33,5 +45,5 @@ app.get("/", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`server running at port: ${port}`);
+  console.log(`Server running at port: ${port}`);
 });
